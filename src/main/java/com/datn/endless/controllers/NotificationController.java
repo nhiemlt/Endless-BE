3dee63ce4ca8a826e7cd9a0955ded91deb5dab6b
nhiemlt <lytinhnhiem@gmail.com>
2024-09-26 17:27:11 +0700

    setting for security

--- src/main/java/com/datn/endless/controllers/NotificationController.java
+++ src/main/java/com/datn/endless/controllers/NotificationController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/notifications")
+@RequestMapping("/notifications")
 public class NotificationController {
 
     @Autowired
