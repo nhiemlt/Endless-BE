9a6f585306048774507ec13cb5c641e9ba708da7
vtthaonguyen <149494249+vtthaonguyen@users.noreply.github.com>
2024-10-23 19:51:57 +0700

    user, voucher - Controller, user, userAddress, voucher - Repository, user, voucher, userAddress - Service

--- src/main/java/com/datn/endless/repositories/UservoucherRepository.java
+++ src/main/java/com/datn/endless/repositories/UservoucherRepository.java
@@ -4,6 +4,10 @@ import com.datn.endless.entities.User;
 import com.datn.endless.entities.Uservoucher;
 import com.datn.endless.entities.Voucher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,4 +17,10 @@ public interface UservoucherRepository extends JpaRepository<Uservoucher, String
     Uservoucher findByUserIDAndVoucherID(User userID, Voucher voucherID);
     // Tìm tất cả UserVoucher của một User
     List<Uservoucher> findByUserID(User userID);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO uservouchers (user_id, voucher_id) " +
+            "SELECT u.user_id, :voucherID FROM users u WHERE u.active = true", nativeQuery = true)
+    void addVoucherToAllActiveUsers(@Param("voucherID") String voucherID);
 }
