65ff6fa95fc59b9bb651724ea15eed5ef0f865bd
nhiemlt <lytinhnhiem@gmail.com>
2024-11-22 13:27:47 +0700

    handle payment by zalopay

--- src/main/java/com/datn/endless/services/ZaloPaymentService.java
+++ src/main/java/com/datn/endless/services/ZaloPaymentService.java
@@ -241,7 +241,7 @@ public class ZaloPaymentService {
                 "<h1 class=\"text-9xl font-black text-gray-200\">" + title + "</h1>" +
                 "<p class=\"text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl\">" + message + "</p>" +
                 "<p class=\"mt-4 text-gray-500\">" + content + "</p>" +
-                "<a href=\"http://localhost:3000/login\" class=\"mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring\">" +
+                "<a href=\"http://localhost:3000\" class=\"mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring\">" +
                 "Đi đến trang đăng nhập" +
                 "</a>" +
                 "</div>" +
